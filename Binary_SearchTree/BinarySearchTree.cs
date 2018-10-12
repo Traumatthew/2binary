fd10c8485f9eac0fca74922978aee339db42dd99
Traumatthew <43039716+Traumatthew@users.noreply.github.com>
2018-10-12 08:57:26 -0500

    first commit

---
+++ Binary_SearchTree/BinarySearchTree.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_SearchTree
+{
+    public class BinarySearchTree
+    {
+        
+        public Node root;
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        public void Add(int data)
+        {
+            Node newNode = new Node();
+            newNode.Data = data;
+            if (root == null)
+                root = newNode;
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (data < current.Data)
+                    {
+                        current = current.left;
+                        if (current == null)
+                        {
+                            parent.left = newNode;
+                            break;
+                        }
+
+                        else
+                        {
+                            current = current.right;
+                            if (current == null)
+                            {
+                                parent.right = newNode;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Binary_SearchTree/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_SearchTree
+{
+    public class Node
+    {
+        public int Data;
+        public Node left;
+        public Node right;
+
+    }
+
+}
